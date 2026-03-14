2611e02833345ad7170ea460f7a70b71f219a0e9
Filip <115399354+alfacentaur1@users.noreply.github.com>
2026-03-14 19:31:13 +0100

    fix init values to be more profitable

--- js/config/plantsConfig.js
+++ js/config/plantsConfig.js
@@ -4,41 +4,41 @@ export const PLANTS_CONFIG = {
     rarity: 'common', 
     image: '../img/tomato.png', 
     growthTime: 15, 
-    sellPrice: 12 
+    sellPrice: 40 
   },
   WHEAT: { 
     name: 'Wheat', 
     rarity: 'common', 
     image: '../img/wheat.png', 
     growthTime: 20, 
-    sellPrice: 8 
+    sellPrice: 60
   },
   PEPPER: { 
     name: 'Pepper', 
     rarity: 'uncommon', 
     image: '../img/pepper.png', 
     growthTime: 40, 
-    sellPrice: 20 
+    sellPrice: 100
   },
   ORANGE: { 
     name: 'Orange', 
     rarity: 'rare', 
     image: '../img/orange.png', 
     growthTime: 70, 
-    sellPrice: 45 
+    sellPrice: 200
   },
   BANANA: { 
     name: 'Banana', 
     rarity: 'epic', 
     image: '../img/banana.png', 
     growthTime: 100, 
-    sellPrice: 90 
+    sellPrice: 400
   },
   MUSHROOM: { 
     name: 'Mushroom', 
     rarity: 'legendary', 
     image: '../img/mushroom.png', 
     growthTime: 150, 
-    sellPrice: 200 
+    sellPrice: 800
   }
 };
